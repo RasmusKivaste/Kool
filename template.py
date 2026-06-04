2c08abce8ac4204f2d6967b14d1b21ed52b86e92
Rasmus Kivaste <rasmus.kivaste@voco.ee>
2026-06-04 10:37:38 +0300

    template

--- template.py
+++ template.py
@@ -1,62 +1,60 @@
-from __future__ import annotations
-
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
-class Game(ABC):
-    def __init__(self, number_of_players: int) -> None:
-        self.number_of_players = number_of_players
-        self.current_player = 0
+class CardGame(ABC):
+    def __init__(self, creatures):
+        self.creatures = creatures
 
-    def run(self) -> None:
-        self.start()
-        while not self.have_winner:
-            self.take_turn()
-        print(f"Player {self.winning_player} wins!")
+    # return -1 if both creatures alive or both dead after combat
+    # otherwise, return the index of winning creature
+    def combat(self, c1_index, c2_index):
+        c1 = self.creatures[c1_index]
+        c2 = self.creatures[c2_index]
 
-    @abstractmethod
-    def start(self) -> None:
-        raise NotImplementedError
+        c2_alive = self.hit(c1, c2)   # c1 ründab c2
+        c1_alive = self.hit(c2, c1)   # c2 ründab c1
 
-    @property
-    @abstractmethod
-    def have_winner(self) -> bool:
-        raise NotImplementedError
+        if c1_alive == c2_alive:
+            return -1
 
-    @abstractmethod
-    def take_turn(self) -> None:
-        raise NotImplementedError
+        if c1_alive:
+            return c1_index
 
-    @property
-    @abstractmethod
-    def winning_player(self) -> int:
-        raise NotImplementedError
+        return c2_index
+
+    def hit(self, attacker, defender):
+        pass
 
 
-class Chess(Game):
-    def __init__(self) -> None:
-        super().__init__(2)
-        self.max_turns = 10
-        self.turn = 1
+class TemporaryDamageCardGame(CardGame):
+    def hit(self, attacker, defender):
+        defender.health -= attacker.attack
 
-    def start(self) -> None:
-        print(f"Starting a game of chess with {self.number_of_players} players.")
+        if defender.health <= 0:
+            return False
 
-    @property
-    def have_winner(self) -> bool:
-        return self.turn == self.max_turns
-
-    def take_turn(self) -> None:
-        print(f"Turn {self.turn} taken by player {self.current_player}")
-        self.turn += 1
-        self.current_player = 1 - self.current_player
-
-    @property
-    def winning_player(self) -> int:
-        return self.current_player
+        defender.health += attacker.attack
+        return True
 
 
-if __name__ == "__main__":
-    chess = Chess()
-    chess.run()
+class PermanentDamageCardGame(CardGame):
+    def hit(self, attacker, defender):
+        defender.health -= attacker.attack
+        return defender.health > 0
 
+class Creature:
+    def __init__(self, attack, health):
+        self.attack = attack
+        self.health = health
+
+
+creatures = [Creature(1, 2), Creature(1, 3)]
+
+game = TemporaryDamageCardGame(creatures)
+print(game.combat(0, 1))
+
+creatures = [Creature(1, 2), Creature(1, 3)]
+
+game = PermanentDamageCardGame(creatures)
+print(game.combat(0, 1))
+print(game.combat(0, 1))
